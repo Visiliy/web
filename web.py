e7df7081f708d44900b640c528da75055330e64b
user <irin-shkuratov8@yandex.ru>
2024-02-14 21:59:36 +0300

    /

--- web.py
+++ web.py
@@ -1,7 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    command1 = StringField('id астронавта', validators=[DataRequired()])
+    command2 = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    command3 = StringField('id капитана', validators=[DataRequired()])
+    command4 = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 
 @app.route('/<tile>')
@@ -27,6 +39,16 @@ def list_prof(list):
     diviz = 'И на Марсе будут яблони цвести!'
     return render_template('list.html', title=list,
                            misia=text, diviz=diviz, list=list)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    text = 'Миссия Колонизация Марса'
+    diviz = 'И на Марсе будут яблони цвести!'
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/list_prof/ol')
+    return render_template('login.html', title='Авторизация', form=form, diviz=diviz, misia=text)
 
 
 if __name__ == '__main__':
