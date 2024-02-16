e734fc0307d90684a64199b76e8e1e0b988745f2
user <irin-shkuratov8@yandex.ru>
2024-02-16 15:51:06 +0300

    /

--- form.py
+++ form.py
@@ -19,28 +19,42 @@ def form_sample_get():
                             <title>Пример формы</title>
                           </head>
                           <body>
-                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <center><h1>Анкета предендента</h1></center>
+                            <center><h2>на участие в миссии</h2></center>
                             <div>
                                 <form class="login_form" method="post">
+                                    <input type="text" class="form-control" id="text" placeholder="Введите фамилию" name="text">
+                                    <input type="text" class="form-control" id="text" placeholder="Введите Имя" name="text">
+                                    <h2><br></h2>
                                     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
                                     <div class="form-group">
-                                        <label for="classSelect">В каком вы классе</label>
+                                        <label for="classSelect">Какое у вас образование?</label>
                                         <select class="form-control" id="classSelect" name="class">
-                                          <option>7</option>
-                                          <option>8</option>
-                                          <option>9</option>
-                                          <option>10</option>
-                                          <option>11</option>
+                                          <option>Начальное</option>
+                                          <option>Основное</option>
+                                          <option>Среднее</option>
+                                          <option>Высшее</option>
                                         </select>
                                      </div>
                                     <div class="form-group">
-                                        <label for="about">Немного о себе</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                        <h6><br></h6>
+                                        <label for="about">Какие у вас есть профессии!</label>
+                                        <p><input type="checkbox" name="a"> инженер-исследователь</p>
+                                        <p><input type="checkbox" name="a"> пилот</p>
+                                        <p><input type="checkbox" name="a"> строитель</p>
+                                        <p><input type="checkbox" name="a"> экзобиолог</p>
+                                        <p><input type="checkbox" name="a"> врач</p>
+                                        <p><input type="checkbox" name="a"> инженер по терраформированию</p>
+                                        <p><input type="checkbox" name="a"> климатолог</p>
+                                        <p><input type="checkbox" name="a"> специалист по радиационной защите</p>
+                                        <p><input type="checkbox" name="a"> астрогеолог</p>
+                                        <p><input type="checkbox" name="a"> гляциолог</p>
+                                        <p><input type="checkbox" name="a"> инженер жизнеобеспечения</p>
+                                        <p><input type="checkbox" name="a"> метеоролог</p>
+                                        <p><input type="checkbox" name="a"> оператор марсохода</p>
+                                        <p><input type="checkbox" name="a"> киберинженер</p>
+                                        <p><input type="checkbox" name="a"> штурман</p>
+                                        <p><input type="checkbox" name="a"> пилот дронов</p>
                                     </div>
                                     <div class="form-group">
                                         <label for="form-check">Укажите пол</label>
@@ -57,23 +71,25 @@ def form_sample_get():
                                           </label>
                                         </div>
                                     </div>
+                                    <h6><br></h6>
+                                    <div class="form-group">
+                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
                                     <div class="form-group form-check">
                                         <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
+                                        <label class="form-check-label" for="acceptRules">Готовы ли остаться на Марсе?</label>
                                     </div>
-                                    <button type="submit" class="btn btn-primary">Записаться</button>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
                                 </form>
                             </div>
                           </body>
                         </html>'''
     elif request.method == 'POST':
-        print(request.form['email'])
-        print(request.form['password'])
-        print(request.form['class'])
-        print(request.form['file'])
-        print(request.form['about'])
-        print(request.form['accept'])
-        print(request.form['sex'])
         return "Форма отправлена"
 
 
